d07830b9f9b5ebe260dde185ed294b6658f73e74
Jake Ard <ard20001@byui.edu>
2021-10-21 17:51:47 -0600

    server constant for client

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "disconnected"
-SERVER = socket.gethostbyname(socket.gethostname())
+SERVER = '10.49.179.156'
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
